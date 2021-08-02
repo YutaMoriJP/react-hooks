f71d7e917ff4cf23fb5bf45b1dc0bb97984337d3
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-08-01 17:46:58 -0700

    style udpates

--- src/Hooks/styled.js
+++ src/Hooks/styled.js
@@ -5,6 +5,7 @@ const Wrapper = styled.article`
   flex-direction: column;
   background: ${props => `var(--color-${props.index})`};
   border-radius: 5px;
+  box-shadow: 4px 4px 9px #868e96;
   height: 80px;
   transition: 1s height ease-in-out;
   ${props =>
--- src/styled/Container.js
+++ src/styled/Container.js
@@ -3,13 +3,17 @@ import styled from "styled-components";
 export default styled.article`
   && {
     background: #edf2ff;
-    margin: auto;
-    padding: 10px;
+    margin: 5px auto;
+    border-radius: 8px;
+    box-shadow: 4px 4px 12px #868e96;
+    padding: 20px;
+    width: 1200px;
     max-width: 80%;
     min-width: 180px;
     display: flex;
     flex-direction: column;
     gap: 20px;
+    margin: 5px;
     @media screen and (max-width: 550px) {
       > * {
         margin: 5px;
